be355f24de00c22f6d537130b43420707cdfca48
Yousra <nice.sora@hotmail.com>
2024-09-03 18:37:30 +1000

    fetched the movie data from MTDB API

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,16 +1,36 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
 
+  // Fetching data from TMDB API
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDlkMDFmMjE5MzJkZWZhMzllOWZjZWE5YmQ2N2FjZSIsIm5iZiI6MTcyNTM1MTMwOS43NDY5MDQsInN1YiI6IjY2ZDZjNDUyMmZhMWE0ZTc2MDk0NjhkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c32uI8g7iOpIThb77zWEu5JrQF6Zi3HX2hqHKsQAVaI"
+    }
+  };
+
+  // handleWheel function to scroll the cards
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   };
 
   useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results))
+      .catch((err) => console.error(err));
+
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
 
@@ -18,11 +38,11 @@ const TitleCards = ({ title, category }) => {
     <div className="title-cards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`} alt="" />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
